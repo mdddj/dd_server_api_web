dc5c77df1e7c940be9ce715b5220d9161bc5c565
eee <hlxsmail@gmail.com>
2021-11-09 15:31:40 +0800

    add get order list method

--- src/model/ResourceModel.ts
+++ src/model/ResourceModel.ts
@@ -1,16 +1,18 @@
 import {ResCategory} from "./ResCategory";
+import {User} from "./UserModel";
 
 export interface ResourceModel {
-  title: string;
-  label: string;
-  thumbnailImage: string;
-  createDate: Date;
-  updateDate: Date;
-  description: string;
-  links: string;
-  type: string;
-  clickCount: number;
-  content: string;
-  // 资源所属分类
-  category: ResCategory | undefined;
+    title: string;
+    label: string;
+    thumbnailImage: string;
+    createDate: Date;
+    updateDate: Date;
+    description: string;
+    links: string;
+    type: string;
+    clickCount: number;
+    content: string;
+    // 资源所属分类
+    category: ResCategory | undefined;
+    user?: User
 }
